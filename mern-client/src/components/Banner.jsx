3ff1067c33f06942718f4c21097d5393ccea6ee4
Sumeyye <sumeyyedeg00@gmail.com>
2023-10-13 01:03:28 +0300

    Navbar design was made.

---
+++ mern-client/src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Banner
--- mern-client/src/components/Navbar.jsx
+++ mern-client/src/components/Navbar.jsx
@@ -1,10 +1,71 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaBarsStaggered, FaBlog, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
-    const [isMenuOpen,setMenuOpen]=useState(false)
-    const [isSticky,setSticky]=useState(false)
+    const [isMenuOpen,setIsMenuOpen]=useState(false)
+    const [isSticky,setIsSticky]=useState(false)
+
+    const toggleMenu=()=>{
+        setIsMenuOpen(!isMenuOpen)
+    }
+    useEffect(()=>{
+        const handleScroll=()=>{
+            if(window.scrollY > 100){
+                setIsSticky(true)
+            }
+            else{
+                setIsSticky(false)
+            }
+        }
+        window.addEventListener("scroll",handleScroll)
+        return ()=>{
+            window.addEventListener("scroll",handleScroll)
+        }
+    },[])
+
+    const navItems=[
+        {link:"Home",path:"/"},
+        {link:"About",path:"/about"},
+        {link:"Shop",path:"/shop"},
+        {link:"Sell Your Book",path:"/admin/dashboard"},
+        {link:"Blog",path:"/blog"},
+        
+    ]
   return (
-    <div>Navbar</div>
+    <header className='w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300'>
+        <nav className={`py-4 lg:px-24 px-4 ${isSticky ? "sticky top-0 left-0 right-0 bg-blue-300" : ""}`}>
+            <div className='flex items-center justify-between gap-8'>
+                <Link to="/" className='text-2xl font-bold text-blue-700 flex items-center gap-2'><FaBlog className='inline-block'/>Books</Link>
+                <ul className='md:flex space-x-12 hidden'>
+                    {
+                        navItems.map(({link,path})=><Link key={path} to={path} className='block text-base text-black uppercase cursor-pointer hover:text-blue-700'>{link}</Link>)
+                    }
+                </ul>
+                <div className='space-x-12 hidden lg:flex items-center'>
+                    <button><FaBarsStaggered className='w-5 hover:text-blue-700'/></button>
+                </div>
+                <div className='md:hidden'>
+                    <button onClick={toggleMenu} className='text-black focus:outline-none'>
+                        {
+                            isMenuOpen ? <FaXmark className='h-5 w-5 text-black'/> : <FaBarsStaggered className='h-5 w-5 text-black'/>
+                        }
+                    </button>
+                </div>
+                <div className={`space-y-4 px-4 mt-16 py-7 bg-blue-500 ${isMenuOpen ?  "block fixed top-0 right-0 left-0":"hidden"}`}>
+                    {
+                        navItems.map(({link,path})=><Link key={path} to={path} className='block text-base text-white uppercase cursor-pointer '>{link}</Link>)
+                    }
+                </div>
+            </div>
+
+           
+
+         
+
+           
+        </nav>
+    </header>
   )
 }
 
