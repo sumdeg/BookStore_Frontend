771ca49cfdbfe70e4b0e9cc23478f1ae6bd3bc2e
Sumeyye <sumeyyedeg00@gmail.com>
2023-10-12 23:56:11 +0300

    first commit

---
+++ mern-client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Navbar = () => {
+    const [isMenuOpen,setMenuOpen]=useState(false)
+    const [isSticky,setSticky]=useState(false)
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
