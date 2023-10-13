77925b5e5ba25fb417f08aebee3017f52364e6f0
Sumeyye <sumeyyedeg00@gmail.com>
2023-10-13 23:59:46 +0300

    Banner was made

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import BannerCard from '../home/BannerCard'
 
 const Banner = () => {
   return (
     <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
-        <div>
-            
+        <div className='flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40'>
+            <div className='md:w-1/2 space-y-8 h-full'>
+              <h2 className='text-5xl font-bold leading-snug text-black'>Buy and Sell Your Books <span className='text-blue-700'>for the Best Prices</span></h2>
+              <p className='md:w-4/5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Et amet voluptatum, unde, exercitationem autem tempora facilis doloribus, doloremque enim est veniam cumque 
+                sit excepturi maiores ut saepe? Sequi, eaque fuga!</p>
+                <div>
+                  <input type="text" name='search' id='search' placeholder='Search a book' className='py-2 px-2 rounded-s-sm outline-none' />
+                  <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
+                </div>
+            </div>
+            <div>
+              <BannerCard/>
+            </div>
+
         </div>
     </div>
   )
---
+++ mern-client/src/home/BannerCard.jsx
@@ -0,0 +1,30 @@
+
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import './BannerCard.css'
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+
+import { EffectCards } from 'swiper/modules';
+const BannerCard = () => {
+  return (
+    <div className='banner'>
+        <Swiper
+        effect={'cards'}
+        grabCursor={true}
+        modules={[EffectCards]}
+        className="mySwiper"
+      >
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide></SwiperSlide>
+        
+      </Swiper>
+    </div>
+  )
+}
+
+export default BannerCard
