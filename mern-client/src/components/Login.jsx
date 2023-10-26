5c625e7b6e0e614265bb2bdd1d49de8d80f3ef8f
Sumeyye <sumeyyedeg00@gmail.com>
2023-10-27 00:19:19 +0300

    The login process is coded.

--- mern-client/src/components/Login.jsx
+++ mern-client/src/components/Login.jsx
@@ -1,6 +1,46 @@
 import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const location=useLocation()
+    const navigate=useNavigate()
+    const from=location.state?.from?.pathname || "/";
+    
+      const handleLogin = async (event) =>{
+        event.preventDefault();
+        const form=event.target
+          const userName=form.userName.value
+          const password=form.password.value
+  
+          const authenticationObj={
+              userName,
+              password
+          }
+          console.log(authenticationObj)
+          try {
+            const response = await fetch("https://localhost:7052/api/authentication/login", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(authenticationObj)
+            });
+    
+            if (response.ok) {
+                alert("Login successful!");
+                navigate(from, { replace: true });
+                form.reset();
+            } else {
+                alert("Login failed. Please check your credentials.");
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            alert("An error occurred during login. Please try again later.");
+        }
+      }
+
+
   return (
 
 <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
@@ -14,9 +54,9 @@ const Login = () => {
                 <h1 className="text-2xl text-center font-bold">LOGIN</h1>
 				</div>
 				<div className="divide-y divide-gray-200">
-					<form className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+					<form onSubmit={handleLogin} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                         <div className="relative">
-                            <input id="email" name="email" type="text" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />                              
+                            <input id="userName" name="userName" type="text" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Username" />                              
                         </div>
 						<div className="relative">
 							<input  id="password" name="password" type="password" className="peer h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />							
